445a04e2fbfcdf05afb2cc3e029b247d4fceaebd
Shengying Pan <i@thepans.me>
2014-03-17 17:39:31 -0400

    updated parameter in analyzer

--- EasyMerge/merger/analyzer.py
+++ EasyMerge/merger/analyzer.py
@@ -4,4 +4,4 @@ import merger
 class Analyzer():
     def analyze(self, path):
         print 'Analyzing ' + path + '...'
-        return merger.main(path, 10)
+        return merger.main(path, 10, 4)
